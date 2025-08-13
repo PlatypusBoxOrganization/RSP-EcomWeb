4019a27267f595dc98f34095e27c1cc28e5a6716
Amogh <amoghhambarde123@gmail.com>
2025-08-13 15:36:18 +0530

    added contact us link in home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -142,9 +142,11 @@ const Home = () => {
               title="Demo Video"
               allowFullScreen
             />
+            <Link to="/contact" >
             <button className="mt-4 bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700">
               Contact Us
             </button>
+            </Link>
           </div>
         </div>
       </section>
